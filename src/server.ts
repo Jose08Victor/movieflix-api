480717f0e624c2f95b155d13aa3a635dfa66f88d
José Victor <jose08.victor@gmail.com>
2024-05-11 22:29:35 -0300

    configurações do express

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Home page');
+});
+
+app.listen(port, () => {
+   console.log(`Servidor em execução em http://localhost:${port}`);
+});
