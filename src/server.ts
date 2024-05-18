e0693d33868ecc6a852a4ded0b1c478e2314ed6f
José Victor <jose08.victor@gmail.com>
2024-05-17 21:31:34 -0300

    adicionada mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,30 @@ app.get('/movies', async (_, res) => {
    res.json(movies);
 });
 
+//serve para pegar as requisições do body em json
+app.use(express.json())
+
+app.post('/movies', async (req, res) => {
+   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+   
+   try {
+      await prisma.movie.create({
+         data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date),
+         },
+      });
+   } catch (error) {
+      return res.status(500).send({ message: "Falha ao cadastrar um filme" })
+   }
+
+   res.status(201).send();
+});
+
+
 app.listen(port, () => {
    console.log(`Servidor em execução em http://localhost:${port}`);
 });
